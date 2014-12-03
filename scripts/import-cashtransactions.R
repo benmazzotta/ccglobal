0f09b8b676ae01f772d2ce4d36f9591caa5b7e73
Ben Mazzotta <benjamin.mazzotta@tufts.edu>
2014-12-03 16:24:46 -0500

    Fixed remittance price data from RPW
    Previously, RPW data was too selective. Only recipient countries and only year 2011. Now all four years, averaged, and both origin and destination countries used. For a few countries (Brazil, Ghana, etc...) where both are reported, use the sender's rate and not the recipient's.

--- scripts/import-cashtransactions.R
+++ scripts/import-cashtransactions.R
@@ -26,19 +26,23 @@ levels(fincl$Series.Name)
 setnames(fincl, "X2011","Value")
 table(is.na(fincl$Value))
 
-
+##          Data are in long format with variable names in a column. Replace levels with convenient names.
 levels(fincl$Series.Name) <- c("rec_paymt", "rec_remit", "rec_wage", "acc_active")
 
+##          Reshape the data into wide format.
 fincl <- cast(data=fincl, id.vars=c("Country.Name","Country.Code"), Country.Name + Country.Code ~ Series.Name)
 View(fincl)
 
 #       2. Import remittances received total
 
+##          Search for the remittances variable in WDI
 var.remittances <- WDIsearch("Remittances and compensation")[2,1]
 remit <- WDI(indicator = "BX.TRF.PWKR.CD.DT", country = "all", start = 2011, end=2011, extra=TRUE)
 setnames(remit, var.remittances, "remittances")
 remit <- subset(remit, region!="Aggregates")
 names(remit)
+
+##          Drop lat/long, capitals from WDI extra fields
 remit <- remit[,c("country","remittances","iso3c","iso2c", "region","income","lending", "year")]
 remit <- data.table(remit)
 
@@ -46,6 +50,7 @@ remit <- data.table(remit)
 summary(remit)
 rownames(remit)
 
+##          Convenient to report in millions
 remit[,remit_MM := remittances/10^6]
 setkey(remit, remittances, country)
 tail(remit, 20)
@@ -148,14 +153,15 @@ save(remit, file="national remittances value and volume.Rda")
 # View(rpw11_avgrec)
 # str(remit); str(rpw11_avgrec)
 summary(remit)
+summary(fincl)
+summary(rpw11_avgrec)
 
 ##        Drop Cape Verde observation from remit.dt
-remit<- remit[country !="Cape Verde",]
-
+remit<- remit["country" !="Cape Verde",]
 # 
 # ##        Joins failed because "destination" and "iso3c" used different factor variables for the same country codes.
-# remit[,iso3c := as.character(iso3c)]
-# rpw11_avgrec[,iso3c := as.character(iso3c)]
+remit[,iso3c := as.character(iso3c)]
+rpw11_avgrec[,iso3c := as.character(iso3c)]
 
 sum(duplicated(remit$iso3c))
 sum(duplicated(rpw11_avgrec$iso3c))
@@ -167,19 +173,115 @@ sum(is.na(rpw11_avgrec$iso3c))
 
 setkey(remit, iso3c); setkey(rpw11_avgrec, iso3c)
 # rm(temp)
-remit <- merge(remit, rpw11_avgrec, by="iso3c", all.x=TRUE)
-setkey(remit, country, year)
-str(remit)
+
+##          Merge remittance prices with transaaction volume estimates
+remit <- rpw11_avgrec[remit,]
+# summary(remit)
+
+
+###      You can do the same in {base} but why bother.
+# remit <- merge(remit, rpw11_avgrec, by="iso3c", all.x=TRUE)
+# setkey(remit, country, year)
+# str(remit)
 
 ##        Now calculate fees = cashTRX * destpriceavg
 
 remit[,cashfees := cashTRX * destpriceavg]
 summary(remit$cashfees)
+attr(remit$cashfees, "var.label") <- "Aggregate fees on cash out transactions, millions of USD"
+
+##          But 131 missing obs of 202 possible. Only 114 lack price data and only 74 lack TRX volume;.
+summary(remit$cashfees)
+
+cat("\n *** \n These countries are missing transaction VOLUME.")
+remit[is.na(cashTRX), unique(country)]
+
+cat("\n *** \n These countries are missing transaction PRICES.")
+remit[is.na(destpriceavg), unique(country)]
+
+cat("\n *** \n These countries have complete VOLUME AND PRICES.")
+remit[!is.na(cashfees), unique(country)]
+
+
 
 qplot(cashTRX, cashfees, data=remit, label=iso3c, geom="text", log="xy")
 qplot(payXrem, data=remit, stat="bin")
 qplot(cashTRX, data=remit, stat="bin")
 qplot(destpriceavg, data=remit, stat="bin")
+
+
+##        This is a second approach.
+
+##        price.send = average price of cashout transactions conditional on origin country
+##        price.rec = average price of cashout transactions conditional on destination country
+
+
+
+##        Calculate the average price to send.
+##            filter cashout transactions; take the average by source country
+rpsend <- rpw[cashout==T,mean(cc1.total.cost.., na.rm=T), by=source]
+rpsend[,source:=as.character(source)]
+str(rpsend)
+##            rename
+setnames(rpsend, "source","country.send")
+##            DEFINE; price.send is in USD (notional $200, total cost reported in percent)
+rpsend <- rpsend[,price.send:=2*V1][,.SD,.SDcols=c("country.send","price.send")]
+summary(rpsend)
+attr(rpsend$price.send, "var.label") <- "Price to send a $200 transfer including fees and forex, in USD"
+
+cat("\n ##### \n This is good. There are 32 countries that originate transactions cashed out.")
+length(unique(rpw[cashout==T,as.character(source)]))
+
+
+
+###       Calculate price to receive.
+rpdest <- rpw[cashout==T, mean(cc1.total.cost.., na.rm=T), by=destination]
+rpdest[,destination:=as.character(destination)]
+str(rpdest)
+##            rename
+setnames(rpdest, "destination","country.dest")
+##            DEFINE: price.rec is in USD (notional $200, total costreported in percent)
+rpdest <- rpdest[,price.dest:=2*V1][,.SD, .SDcols=c("country.dest", "price.dest")]
+summary(rpdest)
+attr(rpdest$price.dest, "var.label") <- "Price to receive a $200 transfer including fees and forex, in USD"
+
+##            Check for overlap; there are 9. Prefer the sender's rate.
+collisions <- intersect(rpdest$country.dest, rpsend$country.send)
+##            Write summaries to disk separately for origin and destination countries.
+save(rpsend, file="Remittance price summary by source country.Rda")
+save(rpdest, file="Remittance price summary by destination country.Rda")
+
+# ##            Duplicates remain. 
+# fixme <- data.table(collisions, rpdest[collisions,], rpsend[collisions,])
+# rm(fixme, collisions)
+
+
+##            Merge the sending and receiving data together with rbind()
+setnames(rpsend, names(rpsend), c("iso3c","price"))
+setnames(rpdest, names(rpdest), c("iso3c","price"))
+# str(rbind(rpsend,rpdest))
+remprice <- rbind(rpsend,rpdest[!collisions,])
+
+
+setkey(remprice,iso3c)
+remprice[collisions,]
+
+##            _remprice_ is a data table with cash remittance prices averaged 2011-2014
+str(remprice)
+save(remprice, file="Remittance prices worldwide global summary.Rda")
+
+##            clean up
+rm(rpsend, rpdest, collisions)
+rm(rpw, rpw11_avgrec)
+
+
+
+##          Merge with the remittances data
+remit[,iso3c:=as.character(iso3c)]
+setkey(remprice, iso3c); setkey(remit, iso3c)
+tables()
+
+remit <- remprice[remit,]
 
 
 ##        There are 171 missing values for destpriceavg. 
@@ -190,4 +292,5 @@ summary(remit)
 save(remit, file="remittances.Rda")
 
 save.image("remittances.Rdata")
-save.image("archive 20141001.Rdata")
+# save.image("archive 20141001.Rdata")
+# save.image("archive 20141203.Rdata")
---
+++ scripts/lm-feesandtransit.R
@@ -0,0 +1,15 @@
+# #####
+# Model HH transit and fees for cash transactions
+# Ben MAzzotta
+# December 3, 2014
+# #####
+
+
+# #####
+# Goals.
+# 1. Estimate HH fees for cash transactions
+# 2. Estimate HH transit costs for cash transactions
+# 3. Estimate national time spent on cash transactions
+# 4. Evaluate that time at national median wage
+# #####
+
