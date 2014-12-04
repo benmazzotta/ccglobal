3884b86a8fa40c7c0a37b0a2ce45194bdc7c1e0a
Ben Mazzotta <benjamin.mazzotta@tufts.edu>
2014-12-04 16:21:04 -0500

    Household costs--- fees and transit
    Summary PDF document with charts and tables written with Rmarkdown, ggplot2 and stargazer.

--- scripts/lm-feesandtransit.R
+++ scripts/lm-feesandtransit.R
@@ -36,6 +36,7 @@ require(ggplot2); require(Hmisc); require(WDI)
 load("remittances.Rda")
 remit[,cashfees:=price*cashTRX]
 summary(remit$cashfees)
+setattr(remit$cashfees, "var.label","National cost of cash access fees, USD millions")
 
 cat("### \n These 81 countries have estimates of cash fees.")
 remit[!is.na(cashfees), country]
@@ -82,22 +83,69 @@ prices <- prices[!is.na(region)]
 prices[,mextransit:=transit$usd]
 summary(prices)
 
+##        Adjust MEX transit cost to local prices
 prices[,transUSD := mextransit*PA.NUS.PPPC.RF]
 attr(prices$transUSD, "var.label") <- "Cost of transit at local prices, per transaction, in USD"
 
 ##        Join transit costs to remittances
 setkey(remit, iso2c)
 tables()
-
 remit <- prices[,.SD,.SDcols=c("iso2c","transUSD")][remit]
 summary(remit)
+
+
+##        Clean up the workspace
+rm(prices, time, transit, trxtime, india.dt)
+
+##        Transit costs
+
+##        Create a new object in memory: fees paid for transit
+hhtransit <- hhtime[,.SD, .SDcols=c("iso2c","Country", "Region","Lending","cashTRX","ATMd","RAI","RAInorm","Population","Rural.Pop")]
+
+##        Join the national per-transaction transit costs
+hhtransit <- remit[,.SD,.SDcols=c("iso2c","transUSD")][hhtransit]
+
+##        Mexico transit incidence is 23.31% of cash transactions recalled. Not sure about per capita rates; we could break this out.
+hhtransit[,transrateMEX := .2331]
+
+##        Model national transit rate adjusted by RAInorm
+hhtransit[,transrate:= RAInorm * transrateMEX]
+setattr(hhtransit$transrate, "var.label", "Estimated share of cash transactions that require transit expense, as a decimal")
+
+##        Model national cost of transit associated with cash transactions.
+hhtransit[,transcost:=cashTRX*transrate*transUSD]
+setattr(hhtransit$transcost, "var.label", "Estimated national cost of household transit for cash access, in USD (millions)")
+setattr(hhtransit$transUSD, "var.label", "Estimated household transit cost for a single transaction, in USD")
+
+
+# #####
+# Findings
+##        A few quick charts.
+##              Boxplot of national costs by region
+boxplot(transcost~Region, data=hhtransit, log="y")
+boxplot(transcost/Population*1e6 ~ Region, data=hhtransit, log="y")
+
+##              Scatterplots of population vs. national cost
+qplot(Population, transcost, data=hhtransit, geom="text", label=iso2c, log="xy") + facet_wrap(~Region)
+
+summary(hhtransit)
+
+# #####
+# Missing data
+
+##        Calculation of the transit cost rate failed for Australia, Canada, CAR, Singapore, Swaziland, Switzerland
+hhtransit[is.na(transrate), Country]
+
+##        Calculation of the cash transactions total failed for many countries.
+##          Based on sketchy remittance data and Global Findex. Need to estimate where unavailable.
+hhtransit[is.na(cashTRX), Country]
+
 
 ##        Save your work
 
 save(remit, file="household_costs.Rda")
+save(hhtime, file="household_time.Rda")
+save(hhtransit, file="household_transit.Rda")
 
+save.image("working.Rdata")
 
-##        Next up: scale cash costs by incidence of travel relative to MEX.
-##        once that is done, you can calculate C= incidence * prices * TRX. 
-
-#         Regrettably we did that for MX and not IN last week. need to retool for MX.
